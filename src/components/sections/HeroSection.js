a43b0d7971ec5d2c27172f75d17989f60d2a81a8
Mudit Sharma <muditshamz@gmail.com>
2021-01-27 23:50:37 +0530

    Adding Styled Components to HeroSection

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,17 +1,44 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 function HeroSection() {
   return (
-    <>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>I design and code iOS (SwiftUI) apps</h1>
-      <p>
-        The place for some amazing design techniques for Swift UI and indepth
-        hands-on experience on all the walkthroughs of the tutorials on your
-        system!
-      </p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>Design & code iOS apps using SwiftUI</Title>
+          <Description>
+            The place for some amazing design techniques for Swift UI and
+            indepth hands-on experience on all the walkthroughs of the tutorials
+            on your system!
+          </Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 export default HeroSection
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: whitesmoke;
+`
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`
