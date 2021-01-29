ee6f1ed793f746c4ee0bf648fa8d1ed31d5b1bd4
Mudit Sharma <muditshamz@gmail.com>
2021-01-29 14:34:08 +0530

    Start Exploring Button completed with Animation

--- src/components/Buttons/PurchaseButton.js
+++ src/components/Buttons/PurchaseButton.js
@@ -9,8 +9,14 @@ export default function PurchaseButton(props) {
   return (
     <Link to="/page-2">
       <Wrapper>
-        <Title>{title || "Get Pro Access"}</Title>
-        <Subtitle>{subtitle || "$20 per month"}</Subtitle>
+        <IconWrapper>
+          <Icon src="/images/icons/credit.svg" className="icon" />
+          <Ring src="/images/icons/icon-ring.svg" />
+        </IconWrapper>
+        <TextWrapper>
+          <Title>{title || "Get Pro Access"}</Title>
+          <Subtitle>{subtitle || "$20 per month"}</Subtitle>
+        </TextWrapper>
       </Wrapper>
     </Link>
   )
@@ -25,6 +31,30 @@ const Wrapper = styled.div`
     0px 20px 40px rgba(23, 0, 102, 0.2),
     inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
   border-radius: 20px;
+  display: grid;
+  grid-template-columns: 53px auto;
+  align-items: center;
+  gap: 20px;
+
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
+
+  :hover {
+    box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
+      0px 20px 60px rgba(23, 0, 102, 0.5),
+      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
+    transform: translateY(-3px);
+
+    .icon {
+      transform: scale(1.2);
+    }
+  }
+`
+const TextWrapper = styled.div`
+  display: grid;
+  gap: 4px;
 `
 
 const Title = styled(Caption2)`
@@ -34,4 +64,35 @@ const Title = styled(Caption2)`
 const Subtitle = styled(SmallText)`
   color: black;
   opacity: 0.7;
+`
+
+const Icon = styled.img`
+  width: 29px;
+  height: 29px;
+`
+
+const Ring = styled.img`
+  position: absolute;
+  top: -15px;
+  left: -16px;
+  ${Wrapper}:hover & {
+    transform: rotate(20deg) scale(1.2) translate(1px, 1px);
+  }
+`
+
+const IconWrapper = styled.div`
+  width: 45px;
+  height: 45px;
+  background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
+  box-shadow: 0px 10px 20px rgba(182, 153, 255, 0.3);
+  border-radius: 50%;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  justify-self: center;
+  position: relative;
+
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg) brightness(130%) saturate(200%);
+  }
 `
