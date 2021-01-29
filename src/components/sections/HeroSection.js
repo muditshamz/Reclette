c5190f48d7d7cd290d5a38e985d325411fec8c5c
Mudit Sharma <muditshamz@gmail.com>
2021-01-29 22:46:26 +0530

    Animation Section Home with hover effect

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import MockupAnimation from "../animations/MockupAnimation"
 import PurchaseButton from "../Buttons/PurchaseButton"
 import { themes } from "../styles/ColorStyles"
 import { H1, MediumText } from "../styles/TextStyles"
@@ -19,6 +20,7 @@ function HeroSection() {
             subtitle="120+ hours of content"
           />
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   )
@@ -27,12 +29,15 @@ export default HeroSection
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `
 const TextWrapper = styled.div`
   max-width: 360px;
