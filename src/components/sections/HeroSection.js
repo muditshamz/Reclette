24d21e5be9e04784bc805bce7cd881d2cdc30dea
Mudit Sharma <muditshamz@gmail.com>
2021-02-05 11:28:28 +0530

    BackGround Wave form with blur

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,12 +1,14 @@
 import React from "react"
 import styled from "styled-components"
 import MockupAnimation from "../animations/MockupAnimation"
+import WaveBackground from "../backgrounds/WaveBackground"
 import PurchaseButton from "../Buttons/PurchaseButton"
 import { themes } from "../styles/ColorStyles"
 import { H1, MediumText } from "../styles/TextStyles"
 function HeroSection() {
   return (
     <Wrapper>
+      <WaveBackground />
       <ContentWrapper>
         <TextWrapper>
           <Title>Design & code iOS apps using SwiftUI</Title>
@@ -28,7 +30,6 @@ function HeroSection() {
 export default HeroSection
 
 const Wrapper = styled.div`
-  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
   overflow: hidden;
 `
 
